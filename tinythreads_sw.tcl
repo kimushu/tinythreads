a5688895c4b1dd592a85846f9ac0997a3a8f0a6a
kimu_shu <alfvegardrisc@gmail.com>
2017-10-11 17:00:14 +0900

    Bump up version to 1.0

--- tinythreads_sw.tcl
+++ tinythreads_sw.tcl
@@ -13,7 +13,7 @@ set_sw_property display_name "TinyThreads"
 set_sw_property extends_bsp_type HAL
 
 # The version of this software
-set_sw_property version 0.1
+set_sw_property version 1.0
 
 # Localtion in generated BSP
 set_sw_property bsp_subdirectory TINYTH
