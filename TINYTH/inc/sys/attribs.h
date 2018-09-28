3eaef3810db480b3216fd23ab9ae159e0bf51f76
kimu_shu <alfvegardrisc@gmail.com>
2018-09-28 23:45:38 +0900

    Update pic32m support

--- TINYTH/inc/sys/attribs.h
+++ TINYTH/inc/sys/attribs.h
@@ -6,9 +6,9 @@
 #if defined(__tinythreads__) && defined(__PIC32MZ__)
 
 #undef __ISR
-#define __ISR(vector, level)    __ISR_WRAPPED(vector)
+#define __ISR(vector, level)    __ISR_WRAPPED(vector, level)
 
-#define __ISR_WRAPPED(v) \
+#define __ISR_WRAPPED(v, l) \
     __dummy__(); /* Consume leading 'void' keyword */ \
     __asm__( \
         ".section .vector_" #v ", code, keep\n" \
@@ -17,40 +17,16 @@
         ".set nomips16\n" \
         ".ent __vector_dispatch_" #v "\n" \
         "__vector_dispatch_" #v ":\n" \
-        "j  __vector_wrapper_" #v "\n" \
-        "nop\n" \
+        "lui     $k0, %hi(__vector_wrapper_" #v ")\n" \
+        "j       __isr_wrapper_entry_" #l "\n" \
+        "addiu   $k0, $k0, %lo(__vector_wrapper_" #v ")\n" \
         ".end __vector_dispatch_" #v "\n" \
         ".section .text.__vector_wrapper_" #v ", code, keep\n" \
         ".set noreorder\n" \
         ".set noat\n" \
         "__vector_wrapper_" #v ":\n" \
-        __TINYTH_ISR_WRAPPER_BODY \
     ); \
     void __attribute__((nomips16, section(".text.__vector_wrapper_" #v)))
-
-#ifdef __PIC32MZ__
-# define __TINYTH_ISR_WRAPPER_BODY \
-    "addiu  $sp, $sp, -64\n" \
-    "sw     $at,  0($sp)\n" \
-    "sw     $v0,  4($sp)\n" \
-    "sw     $v1,  8($sp)\n" \
-    "sw     $a0, 12($sp)\n" \
-    "sw     $a1, 16($sp)\n" \
-    "sw     $a2, 20($sp)\n" \
-    "sw     $a3, 24($sp)\n" \
-    "sw     $t0, 28($sp)\n" \
-    "sw     $t1, 32($sp)\n" \
-    "sw     $t2, 36($sp)\n" \
-    "sw     $t3, 40($sp)\n" \
-    "sw     $t4, 44($sp)\n" \
-    "sw     $t5, 48($sp)\n" \
-    "sw     $t6, 52($sp)\n" \
-    "sw     $t7, 56($sp)\n" \
-    "sw     $ra, 60($sp)\n" \
-    "la     $ra, tth_pic32mz_isr_epilogue\n"
-#else
-# error "ISR wrapper for TinyThreads is not implemented!"
-#endif
 
 #endif  /* __tinythreads__ && __PIC32MZ__ */
 #endif  /* _TINYTH_SYS_ATTRIBS_H_ */
--- TINYTH/src/tth_arch_pic32mz.c
+++ TINYTH/src/tth_arch_pic32mz.c
@@ -1,6 +1,16 @@
 #include <priv/tth_core.h>
 #include <stdint.h>
 
+#if (TTHREAD_FPU_DELAYED_IN_ISR) || (TTHREAD_DSP_DELAYED_IN_ISR)
+void *tth_pic32m_isr_ctx[TTHREAD_MAX_IPL_COUNT];
+#endif
+#if (TTHREAD_FPU_DELAYED_SWITCH)
+void *tth_pic32m_fpu_ctx;
+#endif
+#if (TTHREAD_DSP_DELAYED_SWITCH)
+void *tth_pic32m_dsp_ctx;
+#endif
+
 void _general_exception_handler2(uint32_t cause, uint32_t status)
 {
   if (((cause & _CP0_CAUSE_EXCCODE_MASK) == (_EXCCODE_SYS << _CP0_CAUSE_EXCCODE_POSITION)) &&
