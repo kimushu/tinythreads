0fbb0fb0fd414bdad493a898134bc26d1b3d549f
kimu_shu <alfvegardrisc@gmail.com>
2020-05-09 12:10:35 +0900

    Add functions for interrupt control

--- target/riscv/poc/utils.h
+++ target/riscv/poc/utils.h
@@ -2,7 +2,30 @@
 #define _UTILS_H_
 
 //================================================================
-// QEMU semihosting
+// Control registers
 //
+static inline int disable_interrupts(void) {
+  int mask = (1u<<3);
+  int prev;
+  __asm volatile("csrrc %0, mstatus, %1": "=r"(prev): "r"(mask));
+  return prev;
+}
+
+static inline void enable_interrupts(int status) {
+  __asm volatile("csrs mstatus, %0":: "r"(status & (1u<<3)));
+}
+
+//================================================================
+// QEMU semihosting (pseudo semihosting)
+//
+void SEMI_PRINT(const char *str)
+{
+  //
+}
+
+void SEMI_EXIT(int code)
+{
+  //
+}
 
 #endif  // _UTILS_H_
